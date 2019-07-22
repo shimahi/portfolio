90e934aea640c669f9ddeb5d204ff0424ca5846e
“shimahi“ <shimahi@paroleise.com>
2019-07-22 23:16:51 +0900

    Hello world

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
 import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+  render() {
+    return(<h1>Hello! World</h1>)
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
