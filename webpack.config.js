90f665542adbe7c3af8961833bf0e20a4824ec75
“shimahi“ <shimahi@paroleise.com>
2019-07-23 01:38:26 +0900

    create-react-app部分をwebpackで再構築

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/docs',
+    filename: 'js/main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ],
+            plugins: ['@babel/plugin-syntax-jsx']
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { url: false }
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css'
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  }
+}
