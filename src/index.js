df28eae3ad7321bf0423713132a7b1b9b6614d1f
“shimahi“ <shimahi@paroleise.com>
2019-07-23 21:06:31 +0900

    router挑戦中...

--- src/index.js
+++ src/index.js
@@ -1,31 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './scss/style.scss';
 
 import Header from './components/Header';
 import Me from './components/Me';
 import Mine from './components/Mine';
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <Header />
-          <Route path='/Me' component={Me}/>
-          <Route path='/Mine' component={Mine}/>
-        </Router>
-
-        {/*<Content />
-        <Footer />*/}
-      </div>
-    );
-  }
-}
+import App from './App'
 
 
 ReactDOM.render(
   <App />,
   document.querySelector('#app')
 );
+
+
+/*
+<Router history={browserHistory}>
+    <Route path="/" component={App} />
+</Router>
+
+*/
