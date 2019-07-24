e093a3d1e42225d4a54dbe5fe3c39579f060d840
“shimahi“ <kzrndsk@gmail.com>
2019-07-24 15:33:43 +0900

    Headerとrouterのコンポーネント分離

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './scss/style.scss';
-import App from './App';
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import Header from './components/Header';
+import Main from './components/Main'
 import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+  render() {
+    return(
+      <Router>
+        <React.Fragment>
+          <Header />
+          <Main />
+        </React.Fragment>
+      </Router>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
