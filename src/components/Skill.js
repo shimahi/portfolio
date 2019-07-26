b10bdc99f2b71dafdc4a4d54c561c7c85ac96643
“shimahi“ <shimahi@paroleise.com>
2019-07-26 16:24:40 +0900

    Skillコンポーネント作成

---
+++ src/components/Skill.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function Tags(props) {
+  return(
+    <ul>
+      {/*TODO:key設置でエラー回避*/
+        props.skills.map((skill) =>
+          <li>{skill}</li>
+        )
+      }
+    </ul>
+  );
+}
+
+function Git(props) {
+  return (
+    <p>
+      {props.git.includes("private") ? "リポジトリ非公開" : (<a href={props.git} target="_blank" rel="noreferrer noopener">GitHub</a>)}
+    </p>
+  )
+}
+
+export default class Skill extends React.Component {
+  render () {
+    return(
+      <div className="container">
+        <div className="mt-4 p-4 border d-flex">
+          <div>
+            <img src={this.props.image} alt={this.props.title}/>
+          </div>
+          <div className="ml-4">
+            <h3>{this.props.title}</h3>
+            <Tags skills={this.props.skills} />
+            <Git git={this.props.git}/>
+            <p>
+              {this.props.content}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
