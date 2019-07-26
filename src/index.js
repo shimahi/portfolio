4b6cefb08a90fffeaab199b81d03172c20b6e335
“shimahi“ <shimahi@paroleise.com>
2019-07-26 17:57:34 +0900

    RouterにbrowserHistory情報を与える

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './scss/style.scss';
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter as BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import Header from './Header';
 import Main from './Main'
 import * as serviceWorker from './serviceWorker';
@@ -10,12 +10,12 @@ import * as serviceWorker from './serviceWorker';
 class App extends React.Component {
   render() {
     return(
-      <Router>
+      <BrowserRouter>
         <React.Fragment>
           <Header />
           <Main />
         </React.Fragment>
-      </Router>
+      </BrowserRouter>
     );
   }
 }
