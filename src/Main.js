89ddbb54049b423d4de4113c0484a4c0e28c8313
“shimahi“ <shimahi@paroleise.com>
2019-07-26 10:56:08 +0900

    タグ部分をコンポーネント化してpropsを配列で渡すようにする

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import '../scss/style.scss';
-import Me from '../pages/Me';
-import Mine from '../pages/Mine';
-import Us from '../pages/Us';
+import './scss/style.scss';
+import Me from './pages/Me';
+import Mine from './pages/Mine';
+import Us from './pages/Us';
 
 export default class Main extends React.Component {
   render() {
