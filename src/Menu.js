ca7877afa6c46ee69022e0ce11a2343912d3a8d4
“shimahi“ <kzrndsk@gmail.com>
2019-07-29 18:25:36 +0900

    activeなnavに色つける

--- src/Menu.js
+++ src/Menu.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Switch } from "react-router-dom";
-
+import { BrowserRouter as Router, NavLink, Switch } from "react-router-dom";
 export default class Menu extends React.Component {
   render() {
+
     return(
       <nav>
         <div className="navigation-items">
-          <Link to="/portfolio">me</Link>
-          <Link to="/portfolio/mine">mine</Link>
-          <Link to="/portfolio/us">us</Link>
+          <NavLink exact={true} to="/portfolio" activeClassName='is-active'>me</NavLink>
+          <NavLink to="/portfolio/mine" activeClassName='is-active'>mine</NavLink>
+          <NavLink to="/portfolio/us" activeClassName='is-active'>us</NavLink>
           <span className="subtitle">Shimahi portfolio</span>
         </div>
       </nav>
