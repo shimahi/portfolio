49304d0580908127133c1022744063b7eaf55780
“shimahi“ <shimahi@paroleise.com>
2019-07-30 00:08:02 +0900

    実績ページコーディング進める

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,49 +1,59 @@
 import React from 'react';
 
-const Title = (props) => <h3 className="product__title">{props.title}</h3>
-
 const Image = (props) => (
   <div className="product__image">
     <img src={process.env.REACT_APP_IMAGE_PATH + 'image/'+ props.image} alt={props.title}/>
+    <a href={props.url}>{props.url}</a>
   </div>
 )
 
-const Tags = (props) => {
-  const skillList = props.skills.map((skill) =>
-    <li key={skill + props.keyParent}>{skill}</li>
+function Property(props) {
+  const Tags = (props) => {
+    const skillList = props.skills.map((skill) =>
+      <li key={skill + props.keyParent}>{skill}</li>
+    )
+    return <ul className="product__assets-property__skills">{skillList}</ul>
+  }
+  const Git = (props) => (
+    <div className="product__assets-property__git">
+      {props.git.includes("private") ? "リポジトリ非公開" : <p><a href={props.git} target="_blank" rel="noreferrer noopener">GitHub↗︎</a></p>}
+    </div>
   )
-  return <ul className="product__skills">{skillList}</ul>
+  return (
+    <div className="product__assets-property">
+      <Tags skills={props.skills} keyParent={props.title} />
+      <Git git={props.git} />
+    </div>
+  )
 }
 
-const Git = (props) => (
-  <div className="product__git">
-    {props.git.includes("private") ? "リポジトリ非公開" : <p><a href={props.git} target="_blank" rel="noreferrer noopener">GitHub</a></p>}
+const Title = (props) => (
+  <div className="product__assets-title">
+    <h3 className="product__assets-title__title">{props.title}</h3>
+    <p className="product__assets-title__subtitle">クリエイターの知識共有サイト</p>
   </div>
-
 )
 
 const Content = (props) => (
   <p className="product__content">
     {props.content.map(content =>
-      <span>{content}<br /></span>
+      <>{content}<br /></>
     )}
   </p>
 )
 
 export default class Product extends React.Component {
   render () {
-    console.log(process.env.REACT_APP_IMAGE_PATH)
     return(
-      <div>
-        <div className="mt-4 p-4 border d-flex">
-          <Image image={this.props.image} title={this.props.title} />
-          <div className="ml-4">
-            <Title title={this.props.title} />
-            <Tags skills={this.props.skills} keyParent={this.props.title} />
-            <Git git={this.props.git}/>
-            <Content content={this.props.content} />
-          </div>
+      <div className="product">
+        <Image image={this.props.image} title={this.props.title} url={this.props.url}/>
+        <div className="product__assets">
+          <Title title={this.props.title} />
+          <Property
+            skills={this.props.skills} keyParent={this.props.title} git={this.props.git}
+          />
         </div>
+        <Content content={this.props.content} />
       </div>
     );
   }
