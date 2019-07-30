a981a8e500719d4fd4d97bccd22a51ed0d1ab58f
“shimahi“ <kzrndsk@gmail.com>
2019-07-30 17:52:25 +0900

    ページ遷移時にスクロール位置を初期化する

--- src/index.js
+++ src/index.js
@@ -5,20 +5,23 @@ import './scss/style.scss';
 import { BrowserRouter as BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import Menu from './Menu';
 import Main from './Main'
+import ScrollToTop from './components/ScrollToTop';
 import * as serviceWorker from './serviceWorker';
 
 class App extends React.Component {
   render() {
     return(
       <BrowserRouter>
-        <React.Fragment>
-          <Menu />
-          <Main />
-          <div className="outline">
-            <div></div>
-            <div></div>
-          </div>
-        </React.Fragment>
+        <ScrollToTop>
+          <React.Fragment>
+            <Menu />
+            <Main />
+            <div className="outline">
+              <div></div>
+              <div></div>
+            </div>
+          </React.Fragment>
+        </ScrollToTop>
       </BrowserRouter>
     );
   }
