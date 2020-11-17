a227cf102f7dee012345b33928c81e0fdc114db9
shimahi <shimahi@paroleise.com>
2020-11-18 01:56:46 +0900

    枠を修正

--- src/components/_layouts/Frame.tsx
+++ src/components/_layouts/Frame.tsx
@@ -2,6 +2,10 @@ import tw, { styled } from 'twin.macro'
 
 export const Frame = styled.main`
   ${tw`relative border-8 border-main border-solid min-h-screen`}
+  &:before {
+    content: '';
+    ${tw`fixed top-0 right-0 h-screen w-2 bg-main`}
+  }
   &:after {
     content: '';
     ${tw`fixed bottom-0 left-0 w-full h-2 bg-main`}
