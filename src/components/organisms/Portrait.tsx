9e79f89bea201d8de1f6b86078bb141e9c94f8ec
shimahi <shimahi@rear.jp>
2020-11-18 15:06:24 +0900

    スタイルのコードを修正

--- src/components/organisms/Portrait.tsx
+++ src/components/organisms/Portrait.tsx
@@ -4,9 +4,9 @@ import shimahi from 'assets/shimahi.jpg'
 export const Portrait = () => {
   return (
     <div
-      tw="w-full h-auto mx-auto md:(mx-0 w-48 h-48) relative clearfix"
       css={css`
         max-width: 270px;
+        ${tw`w-full h-auto mx-auto md:(mx-0 w-48 h-48) relative clearfix`}
         &:after {
           ${tw`absolute block w-10 h-10 z-10 bg-main`}
           right: 20px;
