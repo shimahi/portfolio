5a0b247e23d1575a4811a845c9c1352e7dab9df3
shimahi <shimahi@paroleise.com>
2021-05-26 15:36:40 +0900

    setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,22 @@
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
 import { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from 'lib/style'
-import * as gtag from 'lib'
+// import * as gtag from 'lib'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter()
+  // const router = useRouter()
 
-  useEffect(() => {
-    if (!gtag.existsGaId) return
+  // useEffect(() => {
+  //   if (process.env.NODE_ENV === 'production' && gtag.existsGaId) {
+  //     const handleRouteChange = (path: string) => gtag.pageview(path)
 
-    const handleRouteChange = (path: string) => gtag.pageview(path)
+  //     router.events.on('routeChangeComplete', handleRouteChange)
 
-    router.events.on('routeChangeComplete', handleRouteChange)
-
-    return () => router.events.off('routeChangeComplete', handleRouteChange)
-  })
+  //     return () => router.events.off('routeChangeComplete', handleRouteChange)
+  //   }
+  // })
 
   return (
     <ChakraProvider theme={theme}>
