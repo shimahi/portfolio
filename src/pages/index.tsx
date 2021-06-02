1bf168f513b19439346f82be880f46056df14ea3
shimahi <shimahi@paroleise.com>
2021-06-02 21:15:08 +0900

    fetch失敗次にコンポーネントで再取得するようにした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,19 @@
+import { useState, useEffect } from 'react'
 import { Layout } from 'components/layout'
 import { getPortfolio } from 'lib'
 import { IndexTemplate } from 'components/templates'
 
-export default function Index({ content }: Portfolio) {
+export default function Index({ content: initialContent }: Portfolio) {
+  const [content, setContent] = useState(initialContent)
+
+  useEffect(() => {
+    if (!content) {
+      getPortfolio('top').then((res) => {
+        setContent(res.content)
+      })
+    }
+  }, [content])
+
   return (
     <Layout>
       <IndexTemplate content={content} />
@@ -10,10 +21,11 @@ export default function Index({ content }: Portfolio) {
   )
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const res = await getPortfolio('top')
 
   return {
+    revalidate: 1,
     props: {
       content: res.content,
     },
